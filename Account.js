e70b690b77e2aa280c116deb1a01949493471029
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-02-18 12:21:22 +0100

    opdracht af ZONDER bonus opdracht

--- Account.js
+++ Account.js
@@ -1,2 +1,34 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+// module.exports = 70; opdracht 1
+
+// 2
+class BankAccount{
+    constructor(name, balance, creditLimit) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+    };
+    makeDeposit(depositAmount) {
+        if(depositAmount > 0) {
+            this.balance = this.balance + depositAmount;
+        }
+        // console.log("DEPOSIT", depositAmount, depositAmount < 0);
+    };
+    makeWithdrawal(withdrawalAmount) {
+        const totalCreditLimit = this.balance + this.creditLimit;
+        const dayLimit = 5000;
+
+        if(withdrawalAmount > totalCreditLimit){
+            console.log("Sorry, you can not overdraw your account, YOU need more money sucker");
+        }
+        else if(withdrawalAmount > dayLimit) {
+            console.log("Sorry, not allowed more than your day limit of €5000,-");
+
+        } else {
+            this.balance = this.balance - withdrawalAmount;
+        }
+        // console.log("WithDrawal", withdrawalAmount, totalCredit, withdrawalAmount > totalCredit, withdrawalAmount > dayLimit);
+    };
+}
+
+module.exports = BankAccount;
--- BusinessAccount.js
+++ BusinessAccount.js
@@ -1,2 +1,54 @@
 // Export your class here as module.exports = MyClass
-module.exports = null;
+// module.exports = 70; // opdracht 1 business
+
+class Business {
+    constructor(name, balance, creditLimit, sepaPermission) {
+        this.name = name;
+        this.balance = balance;
+        this.creditLimit = creditLimit;
+        this.sepaPermission = sepaPermission;
+    };
+    makeDeposit(addAmount) {
+        if(addAmount > 0) {
+            this.balance = this.balance + addAmount;
+        };
+        // console.log("Added Amount", addAmount, this.balance + addAmount);
+    };
+    makeWithdrawal(withdrawal) {
+        const businessCreditLimit = this.balance + this.creditLimit;
+        const dailyLimit = 20000;
+        if(withdrawal > businessCreditLimit) {
+            console.log("Sorry you can not withdraw more than you limit.")
+        }
+        else if(withdrawal > dailyLimit) {
+            console.log("Sorry, you can not withdraw more than €20000,-");
+
+        } else {
+            this.balance = this.balance - withdrawal;
+            console.log("Yes you can withdraw from your account")
+        }
+         // console.log("LIMIT", businessCreditLimit, withdrawalAmount);
+        // console.log("Number Withdrawal", withdrawal, this.balance - withdrawal);
+    };
+    sepaInvoice(invoice) {
+        const invoiceMinusCost = invoice * 0.99;
+
+        // console.log("SEPA", invoice, this.sepaPermission, !this.sepaPermission);
+
+        if(!this.sepaPermission) {
+            console.log("Sorry, you need sepaPermission for your deposits");
+        }
+        else if(invoice < 0) {
+            console.log("Sorry, can not deposit a negative amount");
+
+        } else {
+            this.balance = this.balance + invoiceMinusCost;
+            console.log("You balance after invoice cost");
+        }
+
+        // console.log("Invoice", invoice, invoiceMinusCost, this.balance + invoiceMinusCost);
+    };
+
+}
+
+module.exports = Business;
